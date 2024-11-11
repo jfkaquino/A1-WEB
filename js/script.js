ad41cc9134c62f47ccf4b911470c6e29b0a0df03
Jorge <jorgefernandoaquino06@gmail.com>
2024-11-10 22:27:52 -0300

    Novo CSS

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("voltar_topo").style.display = "block";
+        document.getElementById("voltar_topo").style.visibility = "visible";
     } else {
-        document.getElementById("voltar_topo").style.display = "none";
+        document.getElementById("voltar_topo").style.visibility = "hidden";
     }
 }
