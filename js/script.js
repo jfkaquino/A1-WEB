0a641e13d7e8e0f205d8e7b5ac136605f654f8ce
Jorge <jorgefernandoaquino06@gmail.com>
2024-11-25 16:04:43 -0300

    Pacotes e CSS

--- js/script.js
+++ js/script.js
@@ -42,23 +42,42 @@ function trocarBanner() {
         currentIndex = (currentIndex + 1) % banners.length;
     }
 
-    mostrarProximoBanner(); // Mostrar o primeiro banner imediatamente
-    setInterval(mostrarProximoBanner, 7000); // Trocar banner a cada 3 segundos
+    mostrarProximoBanner();
+    setInterval(mostrarProximoBanner, 7000);
 }
 
 document.getElementById("botao_menu").addEventListener("click", function () {
     const item_menu = document.getElementsByClassName("item_menu");
-    const menu = document.getElementsByClassName("menu")[0]; // Acessa o primeiro elemento da classe "menu"
+    const menu = document.getElementsByClassName("menu")[0];
+    const botao_menu = document.getElementById("botao_menu");
 
     if (item_menu[0].style.display === "none" || item_menu[0].style.display === "") {
         menu.style.flexDirection = "column";
         for (let i = 0; i < item_menu.length; i++) {
             item_menu[i].style.display = "list-item";
         }
+        botao_menu.innerHTML = '<i class="bi bi-x"></i>';
     } else {
         for (let i = 0; i < item_menu.length; i++) {
             item_menu[i].style.display = "none";
         }
         menu.style.flexDirection = "row";
+        botao_menu.innerHTML = '<i class="bi bi-list"></i>';
     }
+
+    console.log(botao_menu.innerHTML);
 });
+
+document.getElementById('form_contato').onsubmit = function (event) {
+    event.preventDefault(); // Impede o envio do formulário
+
+    // Coleta os dados do formulário
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let telefone = document.getElementById('telefone').value;
+    let publicidade = document.getElementById('publicidade').value;
+    let assunto = document.getElementById('assunto').value;
+    let mensagem = document.querySelector('textarea[name="mensagem"]').value;
+
+    alert("Nome: " + nome + "\nEmail: " + email + "\nTelefone: " + telefone + "\nPublicidade: " + publicidade + "\nAssunto: " + assunto + "\nMensagem: " + mensagem);
+};
